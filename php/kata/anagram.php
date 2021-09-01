bbc1ded3901e6e98df312ca0005bc5f09b246ec3
mehdihasanpour <mehdih761@yahoo.com>
2021-09-01 22:01:13 +0430

    anagram task with count_chars

--- php/kata/anagram.php
+++ php/kata/anagram.php
@@ -15,6 +15,11 @@ function is_anagram(string $s1, string $s2): bool
 function is_anagram2(string $s1, $s2): bool
 {
     // Home work: complete the function using count_chars bultin function
+    $s1 = count_chars($s1, 3);
+
+    $s2 = count_chars($s2, 3);
+
+    return $s1 == $s2;
 }
 
 
